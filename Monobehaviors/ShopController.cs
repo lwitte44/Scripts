bf435236b9f8f875a6fe760fa633311e61e7c23e
lwitte44 <soccerdubbs@gmail.com>
2024-04-16 17:16:39 -0500

    updated for 4/17

--- Monobehaviors/ShopController.cs
+++ Monobehaviors/ShopController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using TMPro;
 using UnityEditor;
 using UnityEditor.SceneManagement;
+using System.IO;
+using System;
 
 public class ShopController : MonoBehaviour
 {
@@ -17,6 +19,29 @@ public class ShopController : MonoBehaviour
     void Start()
     {
         setCount();
+
+        ////////////////
+
+        this.updatePlayerTMP();
+        this.itemTMP.text = "" + ItemsSingleton.steakItemCost;
+
+        //read plain text file
+        this.readItemsData();
+
+        //read json file with serialization
+        string jsonString = this.readItemsDataJson();
+
+        // Parse the JSON string
+        RootObject root = JsonUtility.FromJson<RootObject>(jsonString);
+
+        // Output the data to the console
+        foreach (var item in root.items)
+        {
+            print($"Name: {item.name}, Stat Impacted: {item.stat_impacted}, Modifier: {item.modifier}");
+        }
+
+        /////////////////
+
     }
 
     // Update is called once per frame
@@ -53,4 +78,95 @@ public class ShopController : MonoBehaviour
         yield return new WaitForSeconds(1);
         this.RubyErrorMessage.gameObject.SetActive(false);
     }
+
+ 
+  /////////////////////////
+  
+
+    private void readItemsData()
+    {
+        string filePath = "Assets/Data Files/items_data.txt"; // Path to the file
+        string answer = "";
+
+        // Check if the file exists
+        if (File.Exists(filePath))
+        {
+            try
+            {
+                // Open the file to read from
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    string line;
+                    string[] itemParts = new string[3];
+
+                    int pos = 0;
+                    // Read and display lines from the file until the end of the file is reached
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        string[] parts = line.Split(",");
+                        for (int i = 0; i < parts.Length; i++)
+                        {
+                            print(parts[i]);
+                            itemParts[pos % 3] = parts[i];
+                            pos++;
+                        }
+                        print("Manually parsed with Item Object");
+                        Item theItem = new Item(itemParts[0], itemParts[1], int.Parse(itemParts[2]));
+                        theItem.display();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Display any errors that occurred during reading the file
+                print("An error occurred while reading the file:");
+                print(ex.Message);
+            }
+        }
+        else
+        {
+            print("The file does not exist.");
+        }
+    }
+
+    private string readItemsDataJson()
+    {
+        string filePath = "Assets/Data Files/items_data_json.txt"; // Path to the file
+        string answer = "";
+
+        // Check if the file exists
+        if (File.Exists(filePath))
+        {
+            try
+            {
+                print("Serialized JSON Parsing");
+                // Open the file to read from
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    string line;
+                    // Read and display lines from the file until the end of the file is reached
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        answer = answer + line;
+                    }
+                    return answer;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Display any errors that occurred during reading the file
+                print("An error occurred while reading the file:");
+                print(ex.Message);
+                return null;
+            }
+        }
+        else
+        {
+            print("The file does not exist.");
+            return null;
+        }
+    }
+
+    ///////////////////
+    
 }
---
+++ Normal Classes/ShopScripts/ItemsSingleton.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemsSingleton : MonoBehaviour
+{
+    public static int steakItemCost = 3;
+}
